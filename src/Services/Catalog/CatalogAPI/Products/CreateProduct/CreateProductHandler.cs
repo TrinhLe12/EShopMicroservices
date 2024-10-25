0acbdd47c3836c0c67aab2cf384f71e867f1562e
Trinh Le <lephuongtrinh1210@gmail.com>
2024-10-25 07:26:47 +0700

    Init Catalog API

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Model;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Create product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //Save to datatbase
+
+
+            //Return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
