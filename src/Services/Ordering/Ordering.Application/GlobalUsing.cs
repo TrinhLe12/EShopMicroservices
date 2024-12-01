af5bf4d6e653b9894d4bc076e529d0ec58f98476
Trinh Le <lephuongtrinh1210@gmail.com>
2024-12-01 10:21:41 +0700

    Ordering.Application - Implement simple event handler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,10 +1,13 @@
 ﻿global using BuildingBlocks.CQRS;
 global using FluentValidation;
+global using MediatR;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Enums;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.ValueObjects.StrongTypeIds;
-global using Ordering.Application.Exceptions;
---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Application.Orders.EventHandlers
+{
+    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent> //Inherit INotificationHandler to handle notificaton (domain event) sent from Infra layer by MediatR
+    {
+        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
